609092ef718d5d2052d9c3f9a983744e06160b9e
udosinachi <udosipat@gmail.com>
2020-10-11 22:21:47 +0100

    Added redux to home screen

--- udostore/src/reducers/productReducers.js
+++ udostore/src/reducers/productReducers.js
@@ -4,10 +4,10 @@ import {
   PRODUCT_LIST_FAIL,
 } from '../constants/productConstants'
 
-export const productListReducer = (state = { product: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { loading: true, product: [] }
+      return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload }
     case PRODUCT_LIST_FAIL:
