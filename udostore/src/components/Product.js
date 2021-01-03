67a478d729a99770a63a36697bef9a7e049e9f02
udosinachi <udosipat@gmail.com>
2021-01-03 19:51:09 +0100

    Category model in backend

--- udostore/src/components/Product.js
+++ udostore/src/components/Product.js
@@ -7,7 +7,11 @@ const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img
+          src={product.image}
+          variant='top'
+          style={{ height: '12rem' }}
+        />
       </Link>
 
       <Card.Body>
